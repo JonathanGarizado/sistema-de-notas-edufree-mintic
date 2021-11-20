4d001ba074a573812a082e5c54387e1e8f527205
JonathanGarizado <84166258+JonathanGarizado@users.noreply.github.com>
2021-11-19 20:07:12 -0500

    Delete sistema-de-notas-edufree-mintic directory

--- sistema-de-notas-edufree-mintic/src/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './estudiante-grupo.repository';
-export * from './grupo.repository';
-export * from './materia.repository';
-export * from './nota.repository';
-export * from './programa-academico.repository';
-export * from './rol.repository';
-export * from './usuario.repository';
