c187feeacd9d760435bdde1e9d689d8907004384
haroldsneyder <ariasjuan85@hotmail.com>
2021-11-29 21:23:50 -0500

    componente login

--- frontend-edufree-mintic/src/app/app.module.ts
+++ frontend-edufree-mintic/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
